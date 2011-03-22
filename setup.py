212da865900604a9acaf7a1835bdd80fb64e4a0c
Marc Hulsman <m.hulsman@tudelft.nl>
2011-03-22 20:27:54 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+import distutils.sysconfig
+from distutils.debug import DEBUG
+import sys
+
+setup(name = 'ClusterStorageLib', 
+    version = '0.1.0', 
+    description = 'Library to handle cluster storage system more robustly',
+    author = 'Marc Hulsman',
+    author_email = 'm.hulsman@tudelft.nl',
+    url ='',
+    packages = [],
+    provides=['cluster_storage'],
+    scripts = ['gcp','gls','gmkdir','grmdir','greplicate','grm','cluster_storage.py']
+)
+
+
